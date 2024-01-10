90617bf377cdf194a19b3d000fbde0eb8f775c9e
DumbIch <dumdumbich@yandex.ru>
2024-01-10 09:25:02 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+/**
+ * @Project : <h3>Default (Template) Project</h3>
+ * @File : ${NAME}
+ * @description
+ * <p>${description}</p>
+ * @author DumbIch
+ * @date 2024-01-10 09:23
+ **/
+fun main() {
+    println("Hello World!")
+}
